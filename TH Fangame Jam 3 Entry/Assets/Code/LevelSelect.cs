e57d1bba7e7d9d02a5a233a2a896e1172e86e2a2
Atrufulgium <atrufulgium@gmail.com>
2019-01-13 09:54:44 +0100

    Basic stage 1-3 tilemap

---
+++ TH Fangame Jam 3 Entry/Assets/Code/LevelSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    private static KeyCode[] keys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
+
+    void Update()
+    {
+        for (int i = 0; i < 9; i++) {
+            if (Input.GetKeyDown(keys[i])) {
+                AudioManager.StartSFX(AudioManager.SFX.Click);
+                Scenes.LoadLevel($"Level {i + 1}");
+            }
+        }
+    }
+}
