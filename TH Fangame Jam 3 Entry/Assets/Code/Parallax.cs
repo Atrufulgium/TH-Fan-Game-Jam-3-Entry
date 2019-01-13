74e0740b1ba09ada4ad7b226138db644ddb2e098
Atrufulgium <atrufulgium@gmail.com>
2019-01-13 14:19:59 +0100

    Parallax background

---
+++ TH Fangame Jam 3 Entry/Assets/Code/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach to main camera
+public class Parallax : MonoBehaviour
+{
+    public Transform target;
+    public float amount;
+
+    Transform tr;
+    Vector3 initialPos;
+
+    private void Start() {
+        tr = transform;
+        initialPos = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = target.position;
+        pos.x = tr.position.x * amount + initialPos.x;
+        pos.y = initialPos.y;
+        target.position = pos;
+    }
+}
